bc2871550d680b233f4fd560d17d22b3809d3bcc
Thiago Sawada <tms1_tms2@hotmail.com>
2024-01-31 16:15:05 -0300

    refactor: create smaller methods

--- import_usage_plans.rb
+++ import_usage_plans.rb
@@ -2,87 +2,91 @@ require "aws-sdk-apigateway"
 require "json"
 require 'optparse'
 
-options = {}
-OptionParser.new do |opts|
-  opts.banner = "Usage: ruby import_usage_plans.rb --region REGION --file FILE"
+def parse_arguments
+  options = {}
+  OptionParser.new do |opts|
+    opts.banner = "Usage: ruby import_usage_plans.rb --region REGION --file FILE"
 
-  opts.on("--region REGION", "AWS Region") do |region|
-    options[:region] = region
+    opts.on("--region REGION", "AWS Region") { |region| options[:region] = region }
+    opts.on("--file FILE", "Path to the usage_plans.json file") { |file| options[:file] = file }
+  end.parse!
+
+  unless options[:region] && options[:file]
+    puts "Both --region and --file arguments are required"
+    exit
   end
 
-  opts.on("--file FILE", "Path to the usage_plans.json file") do |file|
-    options[:file] = file
+  options
+end
+
+def read_usage_plans(file_path)
+  begin
+    file = File.read(file_path)
+    JSON.parse(file)
+  rescue Errno::ENOENT => e
+    puts "Error: File not found - #{e.message}"
+    exit
+  rescue Errno::EACCES => e
+    puts "Error: File not accessible - #{e.message}"
+    exit
+  rescue JSON::ParserError => e
+    puts "Error: File content is not valid JSON - #{e.message}"
+    exit
+  rescue StandardError => e
+    puts "An unexpected error occurred - #{e.message}"
+    exit
   end
-end.parse!
-
-if !options[:region] || !options[:file]
-  puts "Both --region and --file arguments are required"
-  exit
 end
 
-input_file = options[:file]
+def create_usage_plan(apigateway, plan)
+  throttle_params = plan["throttle"] ? {
+    burst_limit: plan["throttle"]["burstLimit"].to_i,
+    rate_limit: plan["throttle"]["rateLimit"].to_f
+  } : nil
 
-begin
-  file = File.read(input_file)
-  usage_plans = JSON.parse(file)
-rescue Errno::ENOENT => e
-  puts "Error: File not found - #{e.message}"
-  exit
-rescue Errno::EACCES => e
-  puts "Error: File not accessible - #{e.message}"
-  exit
-rescue JSON::ParserError => e
-  puts "Error: File content is not valid JSON - #{e.message}"
-  exit
-rescue StandardError => e
-  puts "An unexpected error occurred - #{e.message}"
-  exit
+  quota_params = plan["quota"] ? {
+    limit: plan["quota"]["limit"].to_i,
+    offset: plan["quota"]["offset"].to_i,
+    period: plan["quota"]["period"]
+  } : nil
+
+  apigateway.create_usage_plan({
+    name: plan["name"],
+    throttle: throttle_params,
+    quota: quota_params
+  })
 end
 
+def delete_file(file_path)
+  begin
+    File.delete(file_path)
+    puts "#{file_path} has been successfully deleted."
+  rescue Errno::ENOENT
+    puts "Error: The file #{file_path} does not exist."
+  rescue StandardError => e
+    puts "An error occurred while deleting the file #{file_path}: #{e.message}"
+  end
+end
+
+options = parse_arguments
+usage_plans = read_usage_plans(options[:file])
 apigateway = Aws::APIGateway::Client.new(region: options[:region])
 
-begin
-  usage_plans_count = 0
-  error_count = 0
+usage_plans_count = 0
+error_count = 0
 
-  usage_plans["items"].each do |plan|
-    throttle_params = plan["throttle"] ? {
-      burst_limit: plan["throttle"]["burstLimit"].to_i,
-      rate_limit: plan["throttle"]["rateLimit"].to_f
-    } : nil
-
-    quota_params = plan["quota"] ? {
-      limit: plan["quota"]["limit"].to_i,
-      offset: plan["quota"]["offset"].to_i,
-      period: plan["quota"]["period"]
-    } : nil
-
-    begin
-      response = apigateway.create_usage_plan({
-        name: plan["name"],
-        throttle: throttle_params,
-        quota: quota_params
-      })
-      usage_plans_count += 1
-    rescue Aws::APIGateway::Errors::ServiceError => e
-      puts "Failed to create usage plan: #{e.message}"
-      error_count += 1
-    end
-  end
-
-  puts "Total usage plans imported: #{usage_plans_count}"
-  puts "Total errors: #{error_count}"
-
+usage_plans["items"].each do |plan|
   begin
-    puts "Deleting #{input_file}..."
-    File.delete(input_file)
-    puts "#{input_file} has been successfully deleted."
-  rescue Errno::ENOENT
-    puts "Error: The file #{input_file} does not exist."
-  rescue StandardError => e
-    puts "An error occurred while deleting the file #{input_file}: #{e.message}"
+    response = create_usage_plan(apigateway, plan)
+    usage_plans_count += 1
+    puts "Created Usage Plan: #{response.id}"
+  rescue Aws::APIGateway::Errors::ServiceError => e
+    puts "Failed to create usage plan: #{e.message}"
+    error_count += 1
   end
-
-rescue StandardError => e
-  puts "An error occurred: #{e.message}"
 end
+
+puts "Total usage plans imported: #{usage_plans_count}"
+puts "Total errors: #{error_count}"
+
+delete_file(options[:file])
